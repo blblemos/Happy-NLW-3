cbb1f369d3e91e24b5985686dc0ea7996e8a040b
blblemos <blblemos17@gmail.com>
2020-10-16 15:50:26 -0300

    teste hash

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 import './styles/global.css';
 
 import Routes from './routes';
-import Landing from './pages/Landing';
 
 function App() {
   return (
-    /*<Routes />*/
-    <Landing />
+    <Routes />
   );
 }
 
