f39cd76ef495c75efd9c48780cd704ec7643ae2c
blblemos <blblemos17@gmail.com>
2020-10-16 15:35:23 -0300

    deploy teste

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './styles/global.css';
 
 import Routes from './routes';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <Routes />
+    /*<Routes />*/
+    <Landing />
   );
 }
 
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -22,9 +22,7 @@ function Landing() {
                     <span>Bahia</span>
                 </div> 
 
-                <Link to="/app" className="enter-app">
-                <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-                </Link>
+                
             </div> 
 
         </div> 
